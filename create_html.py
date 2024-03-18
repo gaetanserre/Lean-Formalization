b2db6b61cc8229f020508d7b9a6ba23daf058941
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-03-18 17:14:27 +0100

    create_html

---
+++ create_html.py
@@ -0,0 +1,20 @@
+import os
+
+# List all directories in the current directory
+PROJECTS_NAME = os.listdir()
+
+for project in PROJECTS_NAME:
+    current_dir = os.getcwd()
+    if os.path.isdir(project) and not project.startswith("."):
+        print("Current Lean project:", project)
+        os.chdir(project)
+        project = project.replace("-", "")
+        # Get path to all Lean files
+        for file in os.listdir(project):
+            if file.endswith(".lean"):
+                lean_file = os.path.join(project, file)
+                # Call Alectryon on each Lean file
+                os.system(
+                    f"alectryon --frontend lean4 {lean_file} --lake lakefile.lean --webpage-style windowed --output-directory html"
+                )
+    os.chdir(current_dir)
